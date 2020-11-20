710fa852ee1b582a50c63540605a79da44594fbf
Nicholas Yang <ny585@nyu.edu>
2020-11-19 23:12:51 -0500

    Got basic EDL parsing working.

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,56 @@ extern crate nom;
 pub mod parser;
 
 fn main() {
-    let input = "   TITLE: ZAMA
-001 AX editchannels edittype clipstarttime clipendtime timelineposition timelinepositionend";
+    let input = "TITLE: Timeline 1   
+FCM: NON-DROP FRAME
+
+001  AX       V     C        00:00:00:00 00:00:01:24 01:00:00:00 01:00:01:24  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+002  AX       V     C        00:00:01:24 00:00:02:02 01:00:01:24 01:00:02:02  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+003  AX       V     C        00:00:02:02 00:00:02:18 01:00:02:02 01:00:02:18  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+004  AX       V     C        00:00:02:18 00:00:02:21 01:00:02:18 01:00:02:21  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+005  AX       V     C        00:00:02:21 00:00:02:25 01:00:02:21 01:00:02:25  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+006  AX       V     C        00:00:02:25 00:00:04:14 01:00:02:25 01:00:04:14  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+007  AX       V     C        00:00:04:14 00:00:04:23 01:00:04:14 01:00:04:23  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+008  AX       V     C        00:00:04:23 00:00:04:28 01:00:04:23 01:00:04:28  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+009  AX       V     C        00:00:04:28 00:00:05:03 01:00:04:28 01:00:05:03  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+010  AX       V     C        00:00:05:03 00:00:06:19 01:00:05:03 01:00:06:19  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+011  AX       V     C        00:00:06:19 00:00:07:00 01:00:06:19 01:00:07:00  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+012  AX       V     C        00:00:07:00 00:00:09:18 01:00:07:00 01:00:09:18  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+013  AX       V     C        00:00:09:18 00:00:09:25 01:00:09:18 01:00:09:25  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+014  AX       V     C        00:00:09:25 00:00:09:29 01:00:09:25 01:00:09:29  
+* FROM CLIP NAME: IMG_2926.TRIM.mov
+
+015  AX       V     C        00:00:09:29 00:00:11:14 01:00:09:29 01:00:11:14  
+* FROM CLIP NAME: IMG_2926.TRIM.mov";
     let res = parser::parse_edl_file(input.as_bytes());
-    println!("{:?}", res);
+    match res {
+        Ok(res) => println!("{:?}", res),
+        Err(e) => println!("ERR: {:?}", e),
+    }
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,16 +1,17 @@
-use nom::bytes::complete::{tag, take_while, take_while1, take_while_m_n};
-use nom::character::complete::{multispace0, space0};
+use nom::branch::alt;
+use nom::bytes::complete::{tag, take_until, take_while, take_while1};
+use nom::character::complete::{char, multispace0, space0};
 use nom::character::{is_alphabetic, is_alphanumeric, is_digit, is_space};
-use nom::error::{Error, ErrorKind, ParseError};
+use nom::error::{Error, ErrorKind};
 use nom::multi::many0;
-use nom::sequence::{delimited, pair};
+use nom::sequence::delimited;
 use nom::Err;
 use nom::IResult;
 
 #[derive(Debug)]
 pub struct EdlFile {
     title: String,
-    clips: Vec<Clip>,
+    lines: Vec<Line>,
 }
 
 #[derive(Debug)]
@@ -28,7 +29,7 @@ pub enum EditChannels {
 pub enum EditType {
     Cut,
     Dissolve,
-    Wipe([u8; 3]),
+    Wipe(u16),
     KeyBackground,
     KeyForeground,
     KRemoveFromForeground,
@@ -45,6 +46,34 @@ pub enum Reel {
 pub struct Clip {
     edit_index: u16,
     reel: Reel,
+    edit_channels: EditChannels,
+    edit_type: EditType,
+    transition_duration: Option<u16>,
+    source_in_time: TimeCode,
+    source_out_time: TimeCode,
+    record_in_time: TimeCode,
+    record_out_time: TimeCode,
+}
+
+#[derive(Debug)]
+pub struct TimeCode {
+    hours: u8,
+    minutes: u8,
+    seconds: u8,
+    frames: u8,
+}
+
+#[derive(Debug)]
+pub enum FrameCodeModeChange {
+    DropFrame,
+    NonDropFrame,
+}
+
+#[derive(Debug)]
+pub enum Line {
+    FCM(FrameCodeModeChange),
+    Clip(Clip),
+    Note(Vec<u8>),
 }
 
 static ZERO_CHAR: u8 = 48;
@@ -53,9 +82,8 @@ pub fn parse_title(input: &[u8]) -> IResult<&[u8], String> {
     let (input, _) = take_while(is_space)(input)?;
     let (input, _) = tag("TITLE:")(input)?;
     let (input, _) = take_while(is_space)(input)?;
-    let (input, title) = take_while_m_n(0, 70, is_alphabetic)(input)?;
-    take_while(is_space)(input)?;
-    Ok((input, String::from_utf8_lossy(title).to_string()))
+    let (input, title) = take_until("\n")(input)?;
+    Ok((input, String::from_utf8_lossy(title).trim().to_string()))
 }
 
 pub fn parse_edit_index(edit_index_str: &[u8]) -> Result<u16, Err<Error<&[u8]>>> {
@@ -74,14 +102,8 @@ pub fn parse_edit_index(edit_index_str: &[u8]) -> Result<u16, Err<Error<&[u8]>>>
 }
 
 pub fn parse_reel(reel_chars: &[u8]) -> Result<Reel, Err<Error<&[u8]>>> {
-    if reel_chars.len() == 4
-        && is_digit(reel_chars[0])
-        && is_digit(reel_chars[1])
-        && is_digit(reel_chars[2])
-    {
-        let tape_index: u16 =
-            ((reel_chars[0] - ZERO_CHAR) * 100 + (reel_chars[1] - ZERO_CHAR) * 10 + reel_chars[2]
-                - ZERO_CHAR) as u16;
+    if reel_chars.len() == 4 {
+        let tape_index = parse_index(reel_chars)?;
         Ok(Reel::Index {
             index: tape_index,
             is_b_roll: reel_chars[3] == b'b',
@@ -101,28 +123,147 @@ macro_rules! ws {
     };
 }
 
-pub fn parse_clip(input: &[u8]) -> IResult<&[u8], Clip> {
-    let (input, edit_index) = ws!(take_while1(is_digit))(input)?;
-    let (input, reel) = ws!(take_while1(is_alphanumeric))(input)?;
-    let (input, edit_channels) = ws!(take_while1(is_alphabetic))(input)?;
-    let (input, edit_type) = ws!(take_while1(is_alphabetic))(input)?;
-    let (input, clip_start_time) = ws!(take_while1(is_alphabetic))(input)?;
-    let (input, clip_end_time) = ws!(take_while1(is_alphabetic))(input)?;
-    let (input, timeline_position) = ws!(take_while1(is_alphabetic))(input)?;
-    let (input, timeline_position_end) = ws!(take_while1(is_alphabetic))(input)?;
+pub fn parse_edit_channels(input: &[u8]) -> Result<EditChannels, Err<Error<&[u8]>>> {
+    match input {
+        b"A" => Ok(EditChannels::Audio1),
+        b"B" => Ok(EditChannels::Audio1Video),
+        b"V" => Ok(EditChannels::Video),
+        b"A2" => Ok(EditChannels::Audio2),
+        b"A2/V" => Ok(EditChannels::Audio2Video),
+        b"AA" => Ok(EditChannels::Audio1Audio2),
+        b"AA/V" => Ok(EditChannels::Audio1Audio2Video),
+        _ => Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1))),
+    }
+}
 
-    let edit_index = parse_edit_index(edit_index)?;
+// Indices in CMX3600 are always 3 digit numbers
+#[inline]
+fn parse_index(input: &[u8]) -> Result<u16, Err<Error<&[u8]>>> {
+    if input.len() == 3 && is_digit(input[0]) && is_digit(input[1]) && is_digit(input[2]) {
+        Ok(
+            ((input[0] - ZERO_CHAR) * 100 + (input[1] - ZERO_CHAR) * 10 + input[2] - ZERO_CHAR)
+                as u16,
+        )
+    } else {
+        Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1)))
+    }
+}
+
+pub fn parse_edit_type(input: &[u8]) -> Result<EditType, Err<Error<&[u8]>>> {
+    match input[0] {
+        b'C' => Ok(EditType::Cut),
+        b'D' => Ok(EditType::Dissolve),
+        b'W' => Ok(EditType::Wipe(parse_index(&input[1..])?)),
+        b'K' => {
+            if input.len() == 1 {
+                Ok(EditType::KeyForeground)
+            } else if input[1] == b'B' {
+                Ok(EditType::KeyBackground)
+            } else if input[1] == b'O' {
+                Ok(EditType::KRemoveFromForeground)
+            } else {
+                Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1)))
+            }
+        }
+        _ => Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1))),
+    }
+}
+
+#[inline]
+fn parse_timecode_elem(input: &[u8], max: u8) -> Result<u8, Err<Error<&[u8]>>> {
+    if input.len() == 2 {
+        let elem = (input[0] - ZERO_CHAR) * 10 + input[1] - ZERO_CHAR;
+        if elem > max {
+            Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1)))
+        } else {
+            Ok(elem)
+        }
+    } else {
+        Err(Err::Error(Error::new(input, ErrorKind::TakeWhile1)))
+    }
+}
+
+fn parse_timecode(input: &[u8]) -> IResult<&[u8], TimeCode> {
+    let (input, hours) = take_while1(is_alphanumeric)(input)?;
+    let (input, _) = char(':')(input)?;
+    let (input, minutes) = take_while1(is_alphanumeric)(input)?;
+    let (input, _) = char(':')(input)?;
+    let (input, seconds) = take_while1(is_alphanumeric)(input)?;
+    let (input, _) = char(':')(input)?;
+    let (input, frames) = take_while1(is_alphanumeric)(input)?;
     Ok((
         input,
-        Clip {
-            edit_index,
-            reel: parse_reel(reel)?,
+        TimeCode {
+            hours: parse_timecode_elem(hours, 23)?,
+            minutes: parse_timecode_elem(minutes, 59)?,
+            seconds: parse_timecode_elem(seconds, 59)?,
+            frames: parse_timecode_elem(frames, 29)?,
         },
     ))
 }
 
+pub fn parse_clip(input: &[u8]) -> IResult<&[u8], Line> {
+    let (input, edit_index) = ws!(take_while1(is_digit))(input)?;
+    let (input, reel) = ws!(take_while1(is_alphanumeric))(input)?;
+    let (input, edit_channels) = ws!(take_while1(is_alphanumeric))(input)?;
+    let (input, edit_type) = ws!(take_while1(is_alphabetic))(input)?;
+    let edit_type = parse_edit_type(edit_type)?;
+    let mut input = input;
+    let transition_duration = match &edit_type {
+        EditType::Cut => None,
+        _ => {
+            let res = ws!(take_while1(is_alphanumeric))(input)?;
+            input = res.0;
+            Some(parse_index(res.1)?)
+        }
+    };
+
+    let (input, source_in_time) = ws!(parse_timecode)(input)?;
+    let (input, source_out_time) = ws!(parse_timecode)(input)?;
+    let (input, record_in_time) = ws!(parse_timecode)(input)?;
+    let (input, record_out_time) = ws!(parse_timecode)(input)?;
+
+    Ok((
+        input,
+        Line::Clip(Clip {
+            edit_index: parse_edit_index(edit_index)?,
+            reel: parse_reel(reel)?,
+            edit_channels: parse_edit_channels(edit_channels)?,
+            transition_duration,
+            edit_type,
+            source_in_time,
+            source_out_time,
+            record_in_time,
+            record_out_time,
+        }),
+    ))
+}
+
+fn parse_fcm(input: &[u8]) -> IResult<&[u8], Line> {
+    let (input, _) = tag("FCM:")(input)?;
+    let (input, mode) = ws!(alt((tag("DROP FRAME"), tag("NON-DROP FRAME"))))(input)?;
+    Ok((
+        input,
+        Line::FCM(match mode {
+            b"DROP FRAME" => FrameCodeModeChange::DropFrame,
+            b"NON-DROP FRAME" => FrameCodeModeChange::NonDropFrame,
+            _ => unreachable!(),
+        }),
+    ))
+}
+
+fn parse_notes(input: &[u8]) -> IResult<&[u8], Line> {
+    let (input, line) = take_until("\n")(input)?;
+    Ok((input, Line::Note(line.into())))
+}
+
 pub fn parse_edl_file(input: &[u8]) -> IResult<&[u8], EdlFile> {
-    let (input, (title, _)) = pair(parse_title, tag("\n"))(input)?;
-    let (input, clips) = many0(delimited(multispace0, parse_clip, multispace0))(input)?;
-    Ok((input, EdlFile { title, clips }))
+    let (input, title) = parse_title(input)?;
+    let (input, _) = char('\n')(input)?;
+    let (input, lines) = many0(delimited(
+        multispace0,
+        alt((parse_clip, parse_fcm, parse_notes)),
+        multispace0,
+    ))(input)?;
+    Ok((input, EdlFile { title, lines }))
 }
